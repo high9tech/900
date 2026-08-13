c8a3553ff9bf1b0601154fde1c781e237cb5bd7c
high9tech <hightechps96@gmail.com>
2026-08-13 17:31:00 +0300

    Delete cache.js

--- cache.js
+++
@@ -1,8 +0,0 @@
-// Service Worker / Cache Management for Offline usage
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('Service Worker Registered successfully.'))
-      .catch(err => console.log('Service Worker registration failed:', err));
-  });
-}
